8a109dea99de60169dde4b7d46cc8c906f16b17f
away-2@1968642178@qq.com <1968642178@qq.com>
2023-07-26 00:36:26 +0800

    li-ji

--- server/controller/mySqlController.js
+++ server/controller/mySqlController.js
@@ -51,9 +51,23 @@ const addGiftItem = (values) => {
   return allService.query(_sql, values)
 }
 
-// 查询礼簿item
+
+
+// 根据follow_id查询礼簿item
 const selectGiftItem = (follow_id) =>{
   let _sql = `select * from addItem where follow_id = ${follow_id};`
+  return allService.query(_sql)
+}
+
+// 查询礼簿
+const selectGiftItemById = () =>{
+  let _sql = `select addGift.item_id, item_name, person_name, item_money from addItem, addGift where addItem.follow_id = addGift.item_id;`
+  return allService.query(_sql)
+}
+
+// 根据person_name查询礼簿item
+const selectGiftItemByName = (person_name) =>{
+  let _sql = `select * from addItem where person_name = ${person_name};`
   return allService.query(_sql)
 }
 
@@ -73,6 +87,8 @@ module.exports = {
     selectGift,
     addGiftItem,
     selectGiftItem,
+    selectGiftItemById,
+    selectGiftItemByName,
     updateGiftItem,
     deleteGiftItem,
    
